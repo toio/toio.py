5a87a9d1a9cee823c5deb7a92f73aea7b5fd5c89
Atsushi Mori <ki4070ma@gmail.com>
2023-09-04 18:08:30 +0900

    Formatted

--- move_toio_by_controller.py
+++ move_toio_by_controller.py
@@ -1,12 +1,11 @@
 import asyncio
-
 import os
 import pprint
+import time
+
 import pygame
 
 from toio import *
-
-import time
 
 
 async def motor_1():
@@ -36,33 +35,33 @@ async def motor_1():
     timeout = 20
     interval = 0.05
     import datetime
+
     s_time = current_time = datetime.datetime.now()
     while (datetime.datetime.now() - s_time).seconds < timeout:
-
         for event in pygame.event.get():
             if event.type == pygame.JOYAXISMOTION:
-                axis_data[event.axis] = round(event.value,2)
+                axis_data[event.axis] = round(event.value, 2)
             elif event.type == pygame.JOYBUTTONDOWN:
                 button_data[event.button] = True
             elif event.type == pygame.JOYBUTTONUP:
                 button_data[event.button] = False
             elif event.type == pygame.JOYHATMOTION:
                 hat_data[event.hat] = event.value
-            
-            os.system('clear')
+
+            os.system("clear")
             pprint.pprint(axis_data)
             if axis_data:
                 l_val, r_val = 0, 0
                 if 1 in axis_data and abs(axis_data[1]) >= 0.1:
-                    l_val = - axis_data[1]
+                    l_val = -axis_data[1]
                 if 3 in axis_data and abs(axis_data[3]) >= 0.1:
-                    r_val = - axis_data[3]
+                    r_val = -axis_data[3]
                 print(l_val, r_val)
-                await cube.api.motor.motor_control(int(r_val*20), int(l_val*20))
+                await cube.api.motor.motor_control(int(r_val * 20), int(l_val * 20))
                 await asyncio.sleep(interval)
 
     # stop
-    print('timeout!')
+    print("timeout!")
     await cube.api.motor.motor_control(0, 0)
 
     await cube.disconnect()
