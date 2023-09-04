1c059391ba2f35a89435d588c96d905bbf963b87
Atsushi Mori <ki4070ma@gmail.com>
2023-09-04 17:08:19 +0900

    Add sample script

---
+++ sample2.py
@@ -0,0 +1,81 @@
+import asyncio
+import sys
+import termios
+import tty
+
+from toio import *
+
+async def scan_and_connect():
+    dev_list = await BLEScanner.scan(num=1)
+    assert len(dev_list)
+    cube = ToioCoreCube(dev_list[0].interface)
+    await cube.connect()
+
+    await asyncio.sleep(3)
+
+    await cube.disconnect()
+    return 0
+
+class ReadChar():
+
+    def __enter__(self):
+
+        self.fd = sys.stdin.fileno()
+        self.old_settings = termios.tcgetattr(self.fd)
+        tty.setraw(sys.stdin.fileno())
+        return sys.stdin.read(1)
+
+    def __exit__(self, type, value, traceback):
+        termios.tcsetattr(self.fd, termios.TCSADRAIN, self.old_settings)
+
+async def motor_1():
+    # connect to a cube
+    dev_list = await BLEScanner.scan(1)
+    assert len(dev_list)
+    cube = ToioCoreCube(dev_list[0].interface)
+    await cube.connect()
+
+    # go
+    timeout = 10
+    interval = 0.1
+    import datetime
+    s_time = current_time = datetime.datetime.now()
+    while (datetime.datetime.now() - s_time).seconds < timeout:
+        with ReadChar() as rc:
+            char = rc
+        if char == 'w':
+            print('w')
+            await cube.api.motor.motor_control(30, 30)
+            await asyncio.sleep(interval)
+        elif char == 'a':
+            print('a')
+            await cube.api.motor.motor_control(-10, 10)
+            await asyncio.sleep(interval)
+        elif char == 'd':
+            print('d')
+            await cube.api.motor.motor_control(10, -10)
+            await asyncio.sleep(interval)
+        elif char == 'x':
+            print('x')
+            await cube.api.motor.motor_control(-30, -30)
+            await asyncio.sleep(interval)
+        elif char == 's':
+            print('s')
+            await cube.api.motor.motor_control(0, 0)
+        if char in "^C":
+            print('finish!')
+            break
+
+
+    # await cube.api.motor.motor_control(30, 30)
+    # await asyncio.sleep(2)
+
+    # stop
+    await cube.api.motor.motor_control(0, 0)
+
+    await cube.disconnect()
+    return 0
+
+if __name__ == "__main__":
+    # asyncio.run(scan_and_connect())
+    asyncio.run(motor_1())
